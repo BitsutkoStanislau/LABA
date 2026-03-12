20a8decf5e695e4eb21448f7ddb74349317a1ee8
unknown <by.stanislau@gmail.com>
2026-03-12 15:26:11 +0300

    almost working

--- lab1.py
+++ lab1.py
@@ -4,22 +4,25 @@ from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv('C:/Users/Asus/my_ml_project/netflix_titles.csv')
 print(df.head())
+
 print(df.info())
-cols = df.columns.tolist()
-print(cols)
-print(df['type'])
-nan_matrix = df.isnull()
-print(nan_matrix)
+print(df.isnull().sum())
 
-director_mode = df['director'].mode()[0]
-cast_mode = df['cast'].mode()[0]
-df = df.fillna({'director': director_mode, 'cast': cast_mode})
-nan_matrix = df.isnull()
-print(nan_matrix)
+for column in df.columns:
+    if df[column].isnull().any():
+        value = df[column].mode()
+        df[column] = df[column].fillna(value[0])
 
+print(df.info())
+print(df.isnull().sum())
+
+num_col = df.select_dtypes(include = ['int64']).columns
+print(df[num_col].head(10))
 scaler = MinMaxScaler()
-scaler.fit(df[['release_year']])
-df[['release_year']] = scaler.transform(df[['release_year']])
+df[num_col] = scaler.fit_transform(df[num_col])
+print(df[num_col].head(10))
 
-df = pd.get_dummies(df, columns = ['country'], drop_first = True)
-print(df.head())
+categ_col = df.select_dtypes(include = ['str']).columns
+df = pd.get_dummies(df, columns = categ_col, drop_first = True)
+
+print(df.head(10))
